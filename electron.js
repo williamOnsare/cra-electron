99514ae3034e453c4dad1016fe90d32659f06e84
William Onsare <william.onsare254@gmail.com>
2023-09-28 20:59:23 +0300

    Create Electron Main File

---
+++ electron.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadFile("build/index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
